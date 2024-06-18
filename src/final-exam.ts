2d3a74eed20dd49d166e963008b05c3cb8b00ded
Your Name <vincent.llauderes@asurion.com>
2024-06-18 17:07:06 +0800

    solution

--- src/final-exam.ts
+++ src/final-exam.ts
@@ -3,23 +3,52 @@ import { uuid } from 'uuidv4';
 /**
  * Challenge #1: Create custom Type
  */
+export type TaskItem = {
+  id: string;
+  task: string;
+  completed?: boolean;
+};
 
 /**
  * Challenge #2: Create an array with custom type
  *
  */
+export let tasks: TaskItem[] = [
+  { id: uuid(), task: 'Buy Groceries' },
+  { id: uuid(), task: 'Watch Netflix' },
+  { id: uuid(), task: 'Read Books' },
+];
 
 /**
  * Challenge #3: Implement add tasks
  */
+export const addItem = (task: string): void => {
+  const newTask: TaskItem = {
+    id: uuid(),
+    task,
+    completed: false,
+  };
+  tasks.push(newTask);
+};
 
 /**
  * Challenge #4: Implement complete tasks
  *
  *
  */
+export const completeItem = (todoId: string): TaskItem[] => {
+  return tasks.map(todo => {
+    if (todo.id === todoId) {
+      todo.completed = true;
+    }
+    return todo;
+  });
+};
 
 /**
  * Challenge #5: Implement clearItems tasks
  *
  */
+export const clearItems = (): void => {
+  tasks = [];
+};
